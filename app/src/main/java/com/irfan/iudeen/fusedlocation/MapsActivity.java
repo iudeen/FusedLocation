4a04786a86d88aafc596ebb6c6c78d98f17f7e48
Irfanuddin <irfanudeen08@gmail.com>
2018-11-21 03:54:40 +0800

    Added Search functionality

--- app/src/main/java/com/irfan/iudeen/fusedlocation/MapsActivity.java
+++ app/src/main/java/com/irfan/iudeen/fusedlocation/MapsActivity.java
@@ -1,9 +1,19 @@
 package com.irfan.iudeen.fusedlocation;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+
+import android.view.View;
+import android.view.WindowManager;
+
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,9 +22,17 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private EditText mSearchText;
+    private static final float DEFAULT_ZOOM = 15f;
+    private ImageView mSearch;
+
 
 
 
@@ -23,10 +41,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        mSearchText = findViewById(R.id.input_search);
+        mSearch = findViewById(R.id.ic_search);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        init();
     }
 
 
@@ -45,10 +66,61 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Intent i = getIntent();
         Location location = i.getParcelableExtra("Location");
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(myLocation).title("My Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
 
+
+    }
+
+    private void init(){
+        Toast.makeText(this, "Welcome to Map", Toast.LENGTH_SHORT ).show();
+
+        mSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                geoLocate();
+                hideSoftKeyboard();
+            }
+    });
+    }
+
+    private void geoLocate(){
+        Toast.makeText(this, "Searching", Toast.LENGTH_SHORT ).show();
+        hideSoftKeyboard();
+
+        String searchString = mSearchText.getText().toString();
+
+        Geocoder geocoder = new Geocoder(MapsActivity.this);
+        List<Address> list = new ArrayList<>();
+        try{
+            list = geocoder.getFromLocationName(searchString, 1);
+        }catch (IOException e){
+            Toast.makeText(this, "Exception", Toast.LENGTH_SHORT).show();
+        }
+
+        if(list.size() > 0){
+            Address address = list.get(0);
+
+
+            moveCamera(new LatLng(address.getLatitude(), address.getLongitude()),DEFAULT_ZOOM, address.getAddressLine(0));
+
+        }
+    }
+    private void moveCamera(LatLng latLng, float zoom, String title){
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+        if(!title.equals("My Location")){
+            MarkerOptions options = new MarkerOptions()
+                    .position(latLng)
+                    .title(title);
+            mMap.addMarker(options);
+        }
+        hideSoftKeyboard();
+    }
+    private void hideSoftKeyboard(){
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
 }
